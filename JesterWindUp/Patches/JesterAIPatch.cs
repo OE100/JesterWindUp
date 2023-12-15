c88c0501af7626457901fe7c957186bb1a02e872
OE.omergunr100 <omer.er10@gmail.com>
2023-12-15 23:33:51 +0200

    Add project files.

---
+++ JesterWindUp/Patches/JesterAIPatch.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace JesterWindUp.Patches
+{
+    [HarmonyPatch(typeof(JesterAI))]
+    internal class JesterAIPatch
+    {
+        private static AudioClip jesterWindUp = null;
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void patchJesterCrankSound(JesterAI __instance)
+        {
+            Plugin.log.LogInfo("Patching JesterAI...");
+            if (jesterWindUp == null)
+            {
+                AssetBundle ab = AssetBundle.LoadFromFile(Paths.PluginPath + "\\OE_Tweaks\\Sounds\\jestersounds");
+                if (ab == null)
+                {
+                    Plugin.log.LogError("Failed to load jestersounds asset bundle");
+                    return;
+                }
+                jesterWindUp = ab.LoadAsset<AudioClip>("jester_wind_up.mp3");
+            }
+        }
+    }
+}
---
+++ JesterWindUp/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JesterWindUp
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(GUID);
+
+        private const string GUID = "oe.tweaks.sounds.jester";
+        private const string NAME = "Jester Wind Up Sound";
+        private const string VERSION = "1.0.0";
+
+        internal static Plugin instance;
+
+        internal static ManualLogSource log;
+
+        private void Awake()
+        {
+            log = this.Logger;
+            log.LogInfo($"'{NAME}' is loading...");
+
+            if (instance == null)
+                instance = this;
+
+            harmony.PatchAll();
+
+            log.LogInfo($"'{NAME}' loaded!");
+        }
+    }
+}
